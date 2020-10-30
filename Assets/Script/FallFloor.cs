99245be78e8b6dd30ba226cc631e6ee24f68a42a
TsukasaOnose <70473575+TsukasaOnose@users.noreply.github.com>
2020-10-30 21:21:08 +0900

    UnityChanAtletic
    20201030
    ステージ作成完了
    UnityChanの動作が、ジャンプキーを押し続けているとバグってしまう。
    着地時に跳ねてisGround判定が何回も起こってしまうのも原因かも？

---
+++ Assets/Script/FallFloor.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallFloor : MonoBehaviour
+{
+    //復元判定
+    private bool isRestor = false;
+    //復元されてからの時間
+    private float restorTime;
+    //点滅間隔
+    private float blinkTime = 0.0f;
+    //レンダラーを入れる変数
+    private Renderer rd = null;
+    //初期地点の座標
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        //レンダラーを取得
+        rd = GetComponent<Renderer>();
+        //初期地点の座標を取得
+        startPos = this.gameObject.transform.position;
+    }
+
+    private void Update()
+    {
+        //復元された時
+        if(isRestor == true)
+        {
+            //明滅　ついている時に戻る
+            if (blinkTime > 0.2f)
+            {
+                //レンダラーを有効化
+                rd.enabled = true;
+                blinkTime = 0.0f;
+            }
+            //明滅　消えているとき
+            else if (blinkTime > 0.1f)
+            {
+                rd.enabled = false;
+            }
+            //明滅　ついている時
+            else
+            {
+                rd.enabled = true;
+            }
+
+            //1秒後たったら明滅終了
+            if (restorTime > 1.0f)
+            {
+                isRestor = false;
+                blinkTime = 0f;
+                restorTime = 0f;
+                rd.enabled = true;
+            }
+            else
+            {
+                blinkTime += Time.deltaTime;
+                restorTime += Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //プレイヤーと接触時
+        if (other.gameObject.CompareTag("GroundCheckTag"))
+        {
+            //1秒後にFall関数を呼ぶ
+            Invoke("Fall", 0.7f);
+        }
+
+        //ゲームオーバーエリアと接触した時
+        else if (other.gameObject.CompareTag("GameOverAreaTag"))
+        {
+            //Restor関数を呼び出す
+            Invoke("Restor", 1);
+        }
+    }
+
+    void Fall()
+    {
+        //リジッドボディのisKinematicを無効化
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    void Restor()
+    {
+        //初期地点に戻す
+        this.gameObject.transform.position = startPos;
+        //isKinematicを有効化
+        GetComponent<Rigidbody>().isKinematic = true;
+        //復元された判定
+        isRestor = true;
+    }
+}
--- Assets/Script/RigidPlayerController.cs
+++ Assets/Script/RigidPlayerController.cs
@@ -29,6 +29,7 @@ public class RigidPlayerController : MonoBehaviour
     private bool isJump = false;
     //GroundCheckScriptの変数
     public GroundCheckScript ground;
+    private float timeToEnableInputs;
 
     void Start()
     {
@@ -52,10 +53,7 @@ public class RigidPlayerController : MonoBehaviour
             isGround = ground.IsGround();
 
             //ジャンプ開始トリガーが戻らない事があるので、毎フレームリセット
-            if (animator.GetBool("JumpStart"))
-            {
-                animator.ResetTrigger("JumpStart");
-            }
+            animator.ResetTrigger("JumpStart");
 
             //前後左右の入力を取得
             input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
@@ -74,49 +72,51 @@ public class RigidPlayerController : MonoBehaviour
                     animator.SetBool("JumpUp", false);
                     //着地アニメーションを再生
                     animator.SetTrigger("JumpLanding");
+                    //行動可能時間を、着地してから0.1 秒後に設定
+                    this.timeToEnableInputs = Time.time + 0.1f;
                 }
 
-                //入力の長さが0.1より大きい時且つ、着地アニメーションが終わっている時
-                if (input.magnitude > 0.1f)
-                {
-                    //入力した方向へ向かせる
-                    transform.LookAt(transform.position + input.normalized);
-                    //アニメーションのSpeedに入力の値を渡す
-                    animator.SetBool("Run", true);
-                    //移動する
-                    velocity += transform.forward * RunSpeed;
-                    //足音を有効化
-                    GetComponent<AudioSource>().volume = 1;
-                }
-                else
-                {
-                    //走りアニメーションを無効化
-                    animator.SetBool("Run", false);
-                    //足音を無効化
-                    GetComponent<AudioSource>().volume = 0;
-                }
-                //ジャンプキーを押した時
-                if (Input.GetKey(KeyCode.Space))
-                {
-                    //ジャンプ開始アニメーションを再生
-                    animator.SetTrigger("JumpStart");
-                    //上方向へジャンプ力を代入
-                    velocity.y += jumpPower;
-                    //ジャンプした位置を記録
-                    jumpPos = transform.position.y;
-                    //ジャンプ判定を有効
-                    isJump = true;
-                }
-                //スペースキーを離した時
-                else
-                {
-                    //ジャンプ上昇アニメーションは無効
-                    animator.SetBool("JumpUp", false);
-                    //ジャンプ判定を無効
-                    isJump = false;
-                    //上に掛かる速度を0
-                    velocity.y = 0;
-                }
+                    //入力の長さが0.1より大きい時且つ、着地アニメーションが終わっている時
+                    if (input.magnitude > 0.1f && Time.time >= timeToEnableInputs)
+                    {
+                        //入力した方向へ向かせる
+                        transform.LookAt(transform.position + input.normalized);
+                        //アニメーションのSpeedに入力の値を渡す
+                        animator.SetBool("Run", true);
+                        //移動する
+                        velocity += transform.forward * RunSpeed;
+                        //足音を有効化
+                        GetComponent<AudioSource>().volume = 1;
+                    }
+                    else
+                    {
+                        //走りアニメーションを無効化
+                        animator.SetBool("Run", false);
+                        //足音を無効化
+                        GetComponent<AudioSource>().volume = 0;
+                    }
+                    //ジャンプキーを押した時
+                    if (Input.GetKey(KeyCode.Space) && Time.time >= timeToEnableInputs)
+                    {
+                        //ジャンプ開始アニメーションを再生
+                        animator.SetTrigger("JumpStart");
+                        //上方向へジャンプ力を代入
+                        velocity.y += jumpPower;
+                        //ジャンプした位置を記録
+                        jumpPos = transform.position.y;
+                        //ジャンプ判定を有効
+                        isJump = true;
+                    }
+                    //スペースキーを離した時
+                    else
+                    {
+                        //ジャンプ上昇アニメーションは無効
+                        animator.SetBool("JumpUp", false);
+                        //ジャンプ判定を無効
+                        isJump = false;
+                        //上に掛かる速度を0
+                        velocity.y = 0;
+                    }
             }
             //着地判定でない時
             else
@@ -176,7 +176,6 @@ public class RigidPlayerController : MonoBehaviour
             }
             //重力を常にかける
             velocity.y += Physics.gravity.y * Time.deltaTime;
-
         }
     }
 
@@ -186,23 +185,14 @@ public class RigidPlayerController : MonoBehaviour
         rigid.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(collision.gameObject.tag);
-        //敵と接触した時
-        if (collision.collider.tag == "EnemyTag")
-        {
-            Debug.Log("敵と接触した");
-        }
-    }
-
     void OnTriggerEnter(Collider other)
     {
         //ゲームオーバーエリアに落ちた時
         if (other.gameObject.tag == "GameOverAreaTag")
         {
-            //ゲームオーバー関数を呼び出す
+            //プレイヤー関数を呼び出す
             GameObject.Find(StageController.STR).GetComponent<StageController>().PlayerDown();
+
         }
         //イベントアイテムに衝突した時
         if (other.gameObject.tag == "EventItemTag")
